993c1b3c1072caf474b0e4188f094f4ab1662196
Lewis Chan <8966669+lwschan@users.noreply.github.com>
2022-12-04 18:47:33 +0800

    Add solution for day 1

--- src/main/java/dev/lewischan/adventofcode/BaseCommand.java
+++ src/main/java/dev/lewischan/adventofcode/BaseCommand.java
@@ -1,7 +1,9 @@
 package dev.lewischan.adventofcode;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.StringWriter;
 import java.nio.charset.Charset;
 import java.time.Year;
@@ -14,41 +16,33 @@ public abstract class BaseCommand {
 
   protected final Year year;
   protected final int day;
-  protected final String commandKey;
-  protected String quizInput;
+  protected final String commandInput;
 
   protected BaseCommand(String commandKey) {
     this.year = Year.parse(commandKey.substring(0, 2), YEAR_FORMATTER);
     this.day = Integer.parseInt(commandKey.substring(3, 5));
-    this.commandKey = commandKey;
+    this.commandInput = "%s.txt".formatted(commandKey);
   }
 
-  public void Run() throws IOException {
-    System.out.printf("Running the solution for year %s day %s%n", year, day);
-
-    quizInput = readQuizInput();
+  public void run() throws IOException {
+    System.out.printf("Running the solution for year %s day %s.%n", year, day);
   }
 
-  private String readQuizInput() throws IOException {
-    var inputStream = getFileAsIOStream("%s.txt".formatted(commandKey));
-
-    return convertToString(inputStream);
-  }
-
-  private InputStream getFileAsIOStream(final String fileName) {
+  protected InputStream getQuizFileAsIOStream() {
     var ioStream = this.getClass()
         .getClassLoader()
-        .getResourceAsStream(fileName);
+        .getResourceAsStream(commandInput);
 
     if (ioStream != null) {
       return ioStream;
     }
-    throw new IllegalArgumentException(fileName + " is not found");
+    throw new IllegalArgumentException("%s could not be found.".formatted(commandInput));
   }
 
-  private String convertToString(InputStream is) throws IOException {
+  protected String getQuizFileAsString() throws IOException {
+    var inputStream = getQuizFileAsIOStream();
     var writer = new StringWriter();
-    IOUtils.copy(is, writer, Charset.defaultCharset());
+    IOUtils.copy(inputStream, writer, Charset.defaultCharset());
     return writer.toString();
   }
 }
--- src/main/java/dev/lewischan/adventofcode/yeartwentytwo/DayOne.java
+++ src/main/java/dev/lewischan/adventofcode/yeartwentytwo/DayOne.java
@@ -1,13 +1,11 @@
 package dev.lewischan.adventofcode.yeartwentytwo;
 
-import java.nio.charset.Charset;
-import org.apache.commons.io.IOUtils;
 import dev.lewischan.adventofcode.BaseCommand;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.Collections;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 
@@ -20,7 +18,52 @@ public class DayOne extends BaseCommand {
 
   @Override
   @ShellMethod(key = CommandKeys.DAY_ONE)
-  public void Run() throws IOException {
-    super.Run();
+  public void run() throws IOException {
+    super.run();
+
+    var ioStream = getQuizFileAsIOStream();
+    var topThreeElvesCalories = new ArrayList<Integer>();
+    topThreeElvesCalories.add(0);
+    topThreeElvesCalories.add(0);
+    topThreeElvesCalories.add(0);
+
+    try (
+        var inputStreamReader = new InputStreamReader(ioStream);
+        var bufferedReader = new BufferedReader(inputStreamReader)
+    ) {
+      String line;
+      int currentElfCalorie = 0;
+
+      while ((line = bufferedReader.readLine()) != null) {
+        if (!line.isEmpty()) {
+          currentElfCalorie += Integer.parseInt(line);
+
+          continue;
+        }
+
+        var leastCalorieCarriedInTopThree = Collections.min(topThreeElvesCalories);
+
+        if (currentElfCalorie > leastCalorieCarriedInTopThree) {
+          topThreeElvesCalories.set(
+              topThreeElvesCalories.indexOf(leastCalorieCarriedInTopThree),
+              currentElfCalorie);
+        }
+
+        currentElfCalorie = 0;
+      }
+
+      ioStream.close();
+
+      System.out.printf("The highest calorie carried by one elf is %s.%n",
+          Collections.max(topThreeElvesCalories));
+
+      var topThreeTotal = 0;
+      for (var calorie : topThreeElvesCalories) {
+        topThreeTotal += calorie;
+      }
+
+      System.out.printf("Total calorie from top 3 elves carrying the most calorie is %s.%n",
+          topThreeTotal);
+    }
   }
 }
